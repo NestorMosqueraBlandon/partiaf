ca945b544934a6d9e4974ccc06acc10d26d3d6e4
Nestor Mosquera <yunsde18@gmail.com>
2021-12-10 01:48:02 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -20,40 +20,40 @@ import { AuthProvider } from './context/auth';
 function App() {
 
   const context = useContext(AuthContext)
-  
-  const {userInfo} = context;
+
+  const { userInfo } = context;
 
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <div className="App">
-        {userInfo? 
-        <Layout>
-          <Switch>
-          <Route path="/" component={BusinessScreen} exact></Route>  
-          <Route path="/store" component={StoreScreen} exact></Route> 
-          <Route path="/home" component={BookingScreen} exact></Route>  
-          <Route path="/booking" component={BookingScreen} exact></Route>  
-          <Route path="/menu" component={MenuScreen} exact></Route>  
-          <Route path="/cover" component={CoverScreen} exact></Route>  
-          <Route path="/buy" component={BuyScreen} exact></Route>  
-          <Route path="/staff" component={StaffScreen} exact></Route>
+      <BrowserRouter>
+        <div className="App">
+          {userInfo ?
+            <Layout>
+              <Switch>
+                <Route path="/" component={BusinessScreen} exact></Route>
+                <Route path="/store" component={StoreScreen} exact></Route>
+                <Route path="/home" component={BookingScreen} exact></Route>
+                <Route path="/booking" component={BookingScreen} exact></Route>
+                <Route path="/menu" component={MenuScreen} exact></Route>
+                <Route path="/cover" component={CoverScreen} exact></Route>
+                <Route path="/buy" component={BuyScreen} exact></Route>
+                <Route path="/staff" component={StaffScreen} exact></Route>
 
-          </Switch>
-          {/* <Route path="/" component={ComingSoon} exact></Route>   */}
-        </Layout>
-        :
+              </Switch>
+              {/* <Route path="/" component={ComingSoon} exact></Route>   */}
+            </Layout>
+            :
 
-        <>
-          <Route path="/" component={HomeScreen} exact></Route>
-          <Route path="/login" component={LoginScreen} exact></Route>
-          <Route path="/register" component={RegisterScreen} exact></Route>  
-          <Route path="/settings" component={SettingsScreen} exact></Route>  
-        </>
-        }
+            <>
+              <Route path="/" component={HomeScreen} exact></Route>
+              <Route path="/login" component={LoginScreen} exact></Route>
+              <Route path="/register" component={RegisterScreen} exact></Route>
+              <Route path="/settings" component={SettingsScreen} exact></Route>
+            </>
+          }
 
-      </div>
-    </BrowserRouter>
+        </div>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
