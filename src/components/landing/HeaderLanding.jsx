5e5f2cff816f141b3ede6844f7314e606fd225b5
Nestor Mosquera <yunsde18@gmail.com>
2022-04-06 16:42:43 -0500

    new update

--- src/components/landing/HeaderLanding.jsx
+++ src/components/landing/HeaderLanding.jsx
@@ -5,14 +5,16 @@ import "../../styles/landing/header.css";
 export default function HeaderLanding() {
   return (
     <header>
-      <div className="landing__logo">
-        <img src="/img/partiaf_logo_standar.svg" alt="" />
+      <div className="main__header">
+        <div className="landing__logo">
+          <img src="/img/partiaf_logo_standar.svg" alt="" />
+        </div>
+        <nav>
+          <Link to="/home">INICIO</Link>
+          <Link to="/home">NOSOTROS</Link>
+          <Link to="/home">CONTACTO</Link>
+        </nav>
       </div>
-      <nav>
-        <Link to="/home">INICIO</Link>
-        <Link to="/home">NOSOTROS</Link>
-        <Link to="/home">CONTACTO</Link>
-      </nav>
 
       <div className="header__text">
         <div>
