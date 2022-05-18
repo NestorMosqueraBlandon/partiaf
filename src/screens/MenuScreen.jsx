8d28c5b69c33fc5816b3596fd7ec947a298711f6
Nestor Mosquera <yunsde18@gmail.com>
2022-05-18 16:42:41 -0500

    update test

--- src/screens/MenuScreen.jsx
+++ src/screens/MenuScreen.jsx
@@ -52,7 +52,7 @@ export default function MenuScreen() {
     );
   };
 
-    // UPLOAD IMAGE HANDLER
+  // UPLOAD IMAGE HANDLER
   const uploadHandler = async (e, imageFIeld = "image") => {
     const file = e.target.files[0];
     const bodyFormData = new FormData();
@@ -75,9 +75,6 @@ export default function MenuScreen() {
     }
   };
 
-
-
-
   // < --------------------Create Item Process  ------------------------->
   //enviar email - storeid, menuId, name, precio
   const itemCreate = useSelector((state) => state.itemCreate);
@@ -91,8 +88,6 @@ export default function MenuScreen() {
   const [amount, setAmount] = useState("");
   const [itemImage, setItemImage] = useState("");
 
-
-  
   const submitCreateItemHandler = (e) => {
     e.preventDefault();
     dispatch(
@@ -119,78 +114,81 @@ export default function MenuScreen() {
   const [openModalUpdateItem, setOpenModalUpdateItem] = useState(false);
   const [nameUpdate, setNameUpdate] = useState();
   const [titleUpdateMenu, setTitleUpdateMenu] = useState();
+  const [descriptionUpdateMenu, setDescriptionUpdateMenu] = useState();
   const [priceUpdate, setPriceUpdate] = useState();
   const [amountUpdate, setAmountUpdate] = useState();
   const [itemImageUpdate, setItemImageUpdate] = useState();
 
   const [nameUpdateMenu, setNameUpdateMenu] = useState();
 
-     // UPLOAD IMAGE HANDLER
-     const uploadUpdateHandler = async (e, imageFIeld = "image") => {
-      const file = e.target.files[0];
-      const bodyFormData = new FormData();
-      bodyFormData.append("file", file);
-      try {
-        dispatch({ type: "UPLOAD_REQUEST" });
-        const { data } = await axios.post(
-          "https://rveapi.herokuapp.com/api/v1/users/upload",
-          bodyFormData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-        dispatch({ type: "UPLOAD_SUCCESS" });
-        setItemImageUpdate(data.secure_url);
-      } catch (err) {
-        console.log(err);
-      }
-    };
+  // UPLOAD IMAGE HANDLER
+  const uploadUpdateHandler = async (e, imageFIeld = "image") => {
+    const file = e.target.files[0];
+    const bodyFormData = new FormData();
+    bodyFormData.append("file", file);
+    try {
+      dispatch({ type: "UPLOAD_REQUEST" });
+      const { data } = await axios.post(
+        "https://rveapi.herokuapp.com/api/v1/users/upload",
+        bodyFormData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      dispatch({ type: "UPLOAD_SUCCESS" });
+      setItemImageUpdate(data.secure_url);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    
   const updateItemHanlder = async (item, menuIdUpdate) => {
     await setItemId(item);
     await setMenuId(menuIdUpdate);
-    setNameUpdate     (item?.name);
-    setPriceUpdate    (item?.price);
-    setAmountUpdate   (item?.amount);
+    setNameUpdate(item?.name);
+    setPriceUpdate(item?.price);
+    setAmountUpdate(item?.amount);
+    setDescriptionUpdateMenu(item?.description);
     setItemImageUpdate(item?.itemImage);
     setOpenModalUpdateItem(true);
-  }
+  };
   const submitUpdateItemHandler = (e) => {
     e.preventDefault();
     dispatch(
       itemsActions.update({
         name: nameUpdate,
+        description: descriptionUpdateMenu,
         price: priceUpdate,
         image: itemImageUpdate,
         amount: amountUpdate,
         email: adminInfo.email,
         storeId: storeInfo.store._id,
         menuId: menuId,
-        _id: itemId._id
+        _id: itemId._id,
       })
     );
   };
 
   const updateMenuHandler = async (menu) => {
-    console.log(menu)
+    console.log(menu);
     await setMenuId(menu._id);
-    setTitleUpdateMenu     (menu?.title);
+    setTitleUpdateMenu(menu?.title);
     setOpenModalUpdate(true);
-  }
+  };
   const submitUpateMenu = (e) => {
     e.preventDefault();
-    dispatch(menuActions.update({
-      _id: menuId,
-      title: titleUpdateMenu,
-      menuId: menuId,
-      email: adminInfo.email,
-      storeId: storeInfo.store._id,
-    }));
-  }
-
+    dispatch(
+      menuActions.update({
+        _id: menuId,
+        title: titleUpdateMenu,
+        menuId: menuId,
+        email: adminInfo.email,
+        storeId: storeInfo.store._id,
+      })
+    );
+  };
 
   // < --------------------delete process  ------------------------->
   const menuDelete = useSelector((state) => state.menuDelete);
@@ -240,7 +238,6 @@ export default function MenuScreen() {
     });
   };
 
-
   useEffect(() => {
     const menuConstants = new constantsTemplate("MENU");
     const itemsConstants = new constantsTemplate("ITEM");
@@ -260,7 +257,6 @@ export default function MenuScreen() {
       setOpenModalUpdateItem(false);
     }
 
-
     if (storeInfo) {
       dispatch(menuActions.list(adminInfo.email, storeInfo.store._id));
     }
@@ -273,7 +269,7 @@ export default function MenuScreen() {
       dispatch({ type: itemsConstants.constants().DELETE_RESET });
     }
 
-    if(successUpdate){
+    if (successUpdate) {
       setOpenModalUpdate(false);
       dispatch({ type: menuConstants.constants().UPDATE_RESET });
     }
@@ -285,10 +281,11 @@ export default function MenuScreen() {
     successCreateItem,
     succesDeleteItem,
     successUpdateItem,
-    successUpdate
+    successUpdate,
   ]);
-  
-  console.log(itemId)
+
+  console.log(itemId);
+  const [menuIdSelected, setMenuIdSelected] = useState();
 
   return (
     <>
@@ -296,81 +293,111 @@ export default function MenuScreen() {
         {loading ? (
           <LoadingBox />
         ) : (
-
           <DragDropContext>
-
-          <div className="center__screen">
-            {menu.length === undefined ? (
-              <h2>NO HAY MENUS, POR FAVOR AÑADE UNO </h2>
-            ) : (
-              <>
-                {menu.map((men) => (
-                  <Droppable key={men._id} droppableId={men._id} >
-                    {(provided) => (
-
-                  <div className="card-t" ref={provided.innerRef} {...provided.droppableProps}> 
-                    <h4>{men.title}</h4>
-
-                    <ul>
-                      {men.items.map((item) => (
-                        <Draggable key={men._id}>
-                          {(provided) => (
-
-                        <li className="menu-item" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >
-                          <img className="img-preview-menu" src={item.image} alt="" />
-                          <p>{item.name} </p>
-
-                          <span className="price"> $ {item.price}</span>
-                          <span className="price"> {item.amount > 0? "DISPONIBLE" : "NO DISPONIBLE"}</span>
-                          <div className="actions">
-                            <button className="image btn-none-new" onClick={() => updateItemHanlder(item, men._id)}>
-                              <i className="bx bx-pencil"></i>
-                            </button>
-                            <button
-                              className="close  btn-none-new"
-                              onClick={() => deleteItemHandler(item, men)}
-                            >
-                              <i className="bx bx-trash"></i>
-                            </button>
-                          </div>
-                        </li>
-                          )}
-
-                        </Draggable>
-
-                      ))}
-                    </ul>
-                    <div className="footer-card-t">
-                      <button onClick={() => AddItem(men._id)}>
-                        <i className="bx bx-plus-medical"></i> Agregar Item
+            <div className="center__screen">
+              {menu.length === undefined ? (
+                <h2>NO HAY MENUS, POR FAVOR AÑADE UNO </h2>
+              ) : (
+                <>
+                  <div className="container-tags">
+                    {menu.map((men) => (
+                      <button onClick={() => setMenuIdSelected(men.title)} className="menu-tag">
+                        <h4>{men.title}</h4>
                       </button>
-                      <div>
-
-                      <button className="image"  onClick={() => updateMenuHandler(men)}>
-                              <i className="bx bx-pencil"></i>
-                        </button>
-                      <button  onClick={() => deleteHandler(men)}>
-                        <i className="bx bxs-trash-alt"></i>
-                      </button>
-                      </div>
-
-                    </div>
-                    {provided.placeholder}
+                    ))}
                   </div>
-                    )}
+                  {menu.filter((men) => men.title.includes(menuIdSelected))
+                  .map((men) => (
+                    <Droppable key={men._id} droppableId={men._id}>
+                      {(provided) => (
+                        <div
+                          className="card-t"
+                          ref={provided.innerRef}
+                          {...provided.droppableProps}
+                        >
+                          <h4>{men.title}</h4>
 
-                  </Droppable>
+                          <ul>
+                            {men.items.map((item) => (
+                              <Draggable key={men._id}>
+                                {(provided) => (
+                                  <li
+                                    className="menu-item"
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                  >
+                                    <img
+                                      className="img-preview-menu"
+                                      src={item.image}
+                                      alt=""
+                                    />
+                                    <p>{item.name} </p>
 
-                ))}
-              </>
-            )}
+                                    <span className="price">
+                                      {" "}
+                                      $ {item.price}
+                                    </span>
+                                    <span className="price">
+                                      {" "}
+                                      {item.amount > 0
+                                        ? "DISPONIBLE"
+                                        : "NO DISPONIBLE"}
+                                    </span>
+                                    <div className="actions">
+                                      <button
+                                        className="image btn-none-new"
+                                        onClick={() =>
+                                          updateItemHanlder(item, men._id)
+                                        }
+                                      >
+                                        <i className="bx bx-pencil"></i>
+                                      </button>
+                                      <button
+                                        className="close  btn-none-new"
+                                        onClick={() =>
+                                          deleteItemHandler(item, men)
+                                        }
+                                      >
+                                        <i className="bx bx-trash"></i>
+                                      </button>
+                                    </div>
+                                  </li>
+                                )}
+                              </Draggable>
+                            ))}
+                          </ul>
+                          <div className="footer-card-t">
+                            <button onClick={() => AddItem(men._id)}>
+                              <i className="bx bx-plus-medical"></i> Agregar
+                              Item
+                            </button>
+                            <div>
+                              <button
+                                className="image"
+                                onClick={() => updateMenuHandler(men)}
+                              >
+                                <i className="bx bx-pencil"></i>
+                              </button>
+                              <button onClick={() => deleteHandler(men)}>
+                                <i className="bx bxs-trash-alt"></i>
+                              </button>
+                            </div>
+                          </div>
+                          {provided.placeholder}
+                        </div>
+                      )}
+                    </Droppable>
+                  ))}
+                </>
+              )}
 
-            <div className="center-extend">
-              <button onClick={() => setOpenModal(!openModal)}>
-              <i className="bx bx-plus-medical"></i> Agregar Menu
-              </button>
+              <div className="center-extend">
+                <button onClick={() => setOpenModal(!openModal)}>
+                  <i className="bx bx-plus-medical"></i> Agregar Menu
+                </button>
+              </div>
             </div>
-          </div>
           </DragDropContext>
         )}
       </div>
@@ -444,7 +471,7 @@ export default function MenuScreen() {
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
-                       <input
+            <input
               type="text"
               placeholder="Descripcion"
               value={description}
@@ -456,14 +483,19 @@ export default function MenuScreen() {
               value={price}
               onChange={(e) => setPrice(e.target.value)}
             />
-              <input
+            <input
               type="number"
               placeholder="Cantidad"
               value={amount}
               onChange={(e) => setAmount(e.target.value)}
             />
-            <input type="file" name="file" id="file"  onChange={(e) => uploadHandler(e, "featuredImage")}  />
-            <img alt="" className="img-preview-modal" src={itemImage}  />
+            <input
+              type="file"
+              name="file"
+              id="file"
+              onChange={(e) => uploadHandler(e, "featuredImage")}
+            />
+            <img alt="" className="img-preview-modal" src={itemImage} />
           </form>
           <div className="modal-footer">
             <button className="btn" onClick={submitCreateItemHandler}>
@@ -479,7 +511,6 @@ export default function MenuScreen() {
         </div>
       </div>
 
-
       {/*MODAL ITEM UPDATE*/}
       <div className={openModalUpdateItem ? "openModal" : "closeModal"}>
         <div className="modal">
@@ -493,20 +524,31 @@ export default function MenuScreen() {
               value={nameUpdate}
               onChange={(e) => setNameUpdate(e.target.value)}
             />
+              <input
+              type="text"
+              placeholder="Descripcion Item"
+              value={descriptionUpdateMenu}
+              onChange={(e) => setDescriptionUpdateMenu(e.target.value)}
+            />
             <input
               type="number"
               placeholder="Precio"
               value={priceUpdate}
               onChange={(e) => setPriceUpdate(e.target.value)}
             />
-              <input
+            <input
               type="number"
               placeholder="Cantidad"
               value={amountUpdate}
               onChange={(e) => setAmountUpdate(e.target.value)}
             />
-            <input type="file" name="file" id="file"  onChange={(e) => uploadUpdateHandler(e, "featuredImage")}  />
-            <img alt="" className="img-preview" src={itemImage}  />
+            <input
+              type="file"
+              name="file"
+              id="file"
+              onChange={(e) => uploadUpdateHandler(e, "featuredImage")}
+            />
+            <img alt="" className="img-preview" src={itemImage} />
           </form>
           <div className="modal-footer">
             <button className="btn" onClick={submitUpdateItemHandler}>
